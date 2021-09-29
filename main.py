8e3f42331bd75c7112f2b5135e68a2f49afd2ce5
Raju Mondal <raju.mondal@winexsoft.com>
2021-09-30 00:30:07 +0600

    Plotting Without Line

--- main.py
+++ main.py
@@ -5,10 +5,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoint = np.array([0, 0])
-ypoint = np.array([0, 250])
+# xpoint = np.array([0, 0])
+# ypoint = np.array([0, 250])
+#
+# plt.plot(xpoint)
+# plt.plot(ypoint)
+# plt.show()
 
-plt.plot(xpoint)
-plt.plot(ypoint)
+xpoint = np.array([1, 8])
+ypoint = np.array([6, 6])
+
+apoint = np.array([4, 4])
+bpoint = np.array([3, 10])
+
+plt.plot(xpoint, ypoint, 'o')
+plt.plot(apoint, bpoint, 'o')
 plt.show()
 
